83dd513aa89521b1a965e4962036eb53c30922f9
John Jacob <balquinjohnjacob@gmail.com>
2026-04-14 19:30:04 +0800

    AR display done, maybe since i cannot test sa device ko. Removed the old AR/QR screens kasi redundant na sila.  Started on the implementation of the quiz,  data structure muna and wala pang quiz logic  also not sure ako kung may catalogue din ang quiz kasi from the main menu diretso sa game screen agad  Additionally, changed the terminology sa "AR Game" -> "Game" lang kasi wala namang ar yung mga game natin

--- app/src/main/java/com/barabad/albayreality/frontend/utilities/data/quizzes/QuizzesModel.kt
+++ app/src/main/java/com/barabad/albayreality/frontend/utilities/data/quizzes/QuizzesModel.kt
@@ -1,4 +1,10 @@
 package com.barabad.albayreality.frontend.utilities.data.quizzes
 
-class QuizzesModel {
-}
+data class QuizzesModel(
+    val question:String,
+    val option1: String,
+    val option2: String,
+    val option3: String,
+    val option4: String,
+    val correctAnswer: String
+)

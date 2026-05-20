ae1fa8210aa5d26e518f3f6dfee2e2a29ac45e1a
John Jacob <balquinjohnjacob@gmail.com>
2026-05-21 01:02:05 +0800

    Experimental: should only show message if there is no model displayed

--- app/src/main/java/com/barabad/albayreality/frontend/screens/ARModeScreen.kt
+++ app/src/main/java/com/barabad/albayreality/frontend/screens/ARModeScreen.kt
@@ -174,11 +174,13 @@ fun ModelDisplay(modelName: String?) {
                 if (node is ModelNode) {
                     node.scale = node.scale * detector.scaleFactor * 0.3f
                 }
-                Toast.makeText(
-                    context,
-                    "If the model disappears, tap twice to place a new model",
-                    Toast.LENGTH_SHORT
-                ).show()
+                if(node == null){
+                    Toast.makeText(
+                        context,
+                        "If the model disappears, tap twice to place a new model",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }, onLongPress = { _, _ ->
                 childNodes.clear()
                 Toast.makeText(context, "Deleted Model", Toast.LENGTH_SHORT).show()
